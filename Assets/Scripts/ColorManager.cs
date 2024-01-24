c55de2360c8eaef46b3934563e87960d0721400d
iticjo <itic.jo@gmail.com>
2024-01-24 16:58:48 +0500

    30 colors

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
-    public enum ColorKey { pink, blue, gold }
+    public enum ColorKey { red, orange, yellow, green, blue, purple, pink, brown, white, black, gray, lime, lightBlue, lilac, magenta, turquoise, maroon, peachy, golden, silver, sparklyRed, sparklyOrange, sparklyGreen, sparklyBlue, sparklyPurple, sparklyPink, sparklyBlack, sparklyLime, sparklyLightBlue, sparklyLilac, sparklyMagenta}
 
     [Serializable]
     private class Color
