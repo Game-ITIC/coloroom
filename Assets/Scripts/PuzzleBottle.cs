5fce1111b39decdf1b2812da6b0660f0d13c05ee
shokhaproitic <152949845+shokhaproitic@users.noreply.github.com>
2024-01-24 15:53:23 +0500

    new code added

--- Assets/Scripts/PuzzleBottle.cs
+++ Assets/Scripts/PuzzleBottle.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class PuzzleBottle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private ColorManager.ColorKey[] stack = new ColorManager.ColorKey[0];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //
 }
--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //saving
+    //last puzzle combination
+    //last puzzle id
+
+    [Serializable] private class Level
     {
-        
+        [Serializable] public struct ColorKeyArray
+        {
+            public ColorManager.ColorKey[] colors;
+        }
+
+        [Serializable] public struct BoolArray
+        {
+            public bool[] bools;
+        }
+
+        public ColorKeyArray[] bottles;
+
+        public int coins = 0;
+
+        public BoolArray[] hidens;
+
+        //drugie prikoli
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Level[] levels;
+
+    private int _levelId;
+    //private int _levelId;
+
+    private void Awake()
     {
-        
+        if (!PlayerPrefs.HasKey("puzzle-level-id")) PlayerPrefs.SetInt("puzzle-level-id", 0);
     }
+
+    private void Start()
+    {
+        OpenPuzzle(PlayerPrefs.GetInt("puzzle-level-id"));
+    }
+
+    private void OpenPuzzle(int id)
+    {
+        //delete last trash
+
+        _levelId = id;
+    }
+
+    //next level ?
 }
