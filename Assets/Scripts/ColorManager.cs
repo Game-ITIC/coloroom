738845041b86a558035f20186f055e986a16a885
iticjo <itic.jo@gmail.com>
2024-01-25 10:07:18 +0500

    Pattern materials

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
-    public enum ColorKey { red, orange, yellow, green, blue, purple, pink, brown, white, black, gray, lime, lightBlue, lilac, magenta, turquoise, maroon, peachy, golden, silver, sparklyRed, sparklyOrange, sparklyGreen, sparklyBlue, sparklyPurple, sparklyPink, sparklyBlack, sparklyLime, sparklyLightBlue, sparklyLilac, sparklyMagenta}
+    public enum ColorKey { red, orange, yellow, green, blue, purple, pink, brown, white, black, gray, lime, lightBlue, lilac, magenta, turquoise, maroon, peachy, golden, silver, sparklyRed, sparklyOrange, sparklyGreen, sparklyBlue, sparklyPurple, sparklyPink, sparklyBlack, sparklyLime, sparklyLightBlue, sparklyLilac, sparklyMagenta, patDots1, patDots2, patDots3, patFlower1, patFlower2, patFlower3, patFlower4, patGeo1, patGeo2, patGeo3, patHeart, patPlaid, patStars1, patStars2}
 
     [Serializable]
     private class Color
