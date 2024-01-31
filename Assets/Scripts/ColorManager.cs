8e734a87468bcb45ca874ce13c643a710aa62bcc
shokhaproitic <152949845+shokhaproitic@users.noreply.github.com>
2024-01-31 15:30:03 +0500

    fixes

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
+    public static ColorManager Instance;
+
     public enum ColorKey { red, orange, yellow, green, blue, purple, pink, brown, white, black, gray, lime, lightBlue, lilac, magenta, turquoise, maroon, peachy, golden, silver, sparklyRed, sparklyOrange, sparklyGreen, sparklyBlue, sparklyPurple, sparklyPink, sparklyBlack, sparklyLime, sparklyLightBlue, sparklyLilac, sparklyMagenta, patDots1, patDots2, patDots3, patFlower1, patFlower2, patFlower3, patFlower4, patGeo1, patGeo2, patGeo3, patHeart, patPlaid, patStars1, patStars2}
 
     [Serializable]
@@ -15,6 +17,11 @@ public class ColorManager : MonoBehaviour
     }
 
     [SerializeField] private Color[] colors;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     public Material GetColor(ColorKey key)
     {
